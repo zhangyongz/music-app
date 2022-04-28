d399f9bce88166e91e4898c004bc5c713f94f64c
zhangyong <zhangyong_web@163.com>
2022-04-28 21:21:20 +0800

    refactor: 添加useMemo、useCallback

--- src/components/audio/AudioPlayer.tsx
+++ src/components/audio/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react"
+import React, { useState, useRef, useEffect, useMemo, useCallback } from "react"
 import { useAppSelector } from '@/store/hooks'
 
 import AudioControls from './AudioControls'
@@ -18,131 +18,116 @@ interface AudioPlayProps {
 
 const AudioPlayer: React.FC<AudioPlayProps> = ({trackIndex, setTrackIndex, audioRef,
     isPlaying, setIsPlaying}) => {
-  // State
-  // const [trackIndex, setTrackIndex] = useState(0);
-  const [trackProgress, setTrackProgress] = useState(0);
-  // const [isPlaying, setIsPlaying] = useState(false);
+  const [trackProgress, setTrackProgress] = useState(0)
   const tracks = useAppSelector(selectTracks)
-  // console.log(tracks);
 
-  // Destructure for conciseness
-  // let title = ''
-  // let artist = ''
-  // let color = ''
-  // let image = ''
-  // let audioSrc = ''
-  // let audioRef = useRef(new Audio(audioSrc));
+  const {title, artist, color, image, audioSrc} = useMemo(() => {
+    const song = tracks[trackIndex] || {}
+    const title = song.name
+    const artist = song.ar?.map((item: artist) => {
+      return item.name
+    }).join(',')
+    const color = ''
+    const image = song.al?.picUrl
+    const audioSrc = audioSrcPrefix + song.id + '.mp3'
+    return {
+      song,title,artist,color,image,audioSrc
+    }
+  }, [tracks, trackIndex])
 
-  // if (tracks[trackIndex]) {
-  const song = tracks[trackIndex] || {};
-  const title = song.name
-  const artist = song.ar?.map((item: artist) => {
-    return item.name
-  }).join(',')
-  const color = ''
-  const image = song.al?.picUrl
-  const audioSrc = audioSrcPrefix + song.id + '.mp3'
-  // Refs
-  // const audioRef = useRef(new Audio(audioSrc));
-  // }
+  const intervalRef = useRef<undefined | number>()
+  const isReady = useRef(false)
 
-  const intervalRef = useRef<undefined | number>();
-  const isReady = useRef(false);
-
-  // Destructure for conciseness
-  const { duration, currentTime } = audioRef.current;
+  const { duration, currentTime } = audioRef.current
 
   const currentPercentage = duration
     ? `${(trackProgress / duration) * 100}%`
-    : "0%";
+    : "0%"
   const trackStyling = `
     -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #c4463a), color-stop(${currentPercentage}, #fff))
-  `;
+  `
 
-  const toPrevTrack = () => {
+  const toPrevTrack = useCallback(() => {
     if (trackIndex - 1 < 0) {
-      setTrackIndex(tracks.length - 1);
+      setTrackIndex(tracks.length - 1)
     } else {
-      setTrackIndex(trackIndex - 1);
+      setTrackIndex(trackIndex - 1)
     }
-  };
+  }, [tracks, trackIndex])
 
-  const toNextTrack = () => {
+  const toNextTrack = useCallback(() => {
     if (trackIndex < tracks.length - 1) {
-      setTrackIndex(trackIndex + 1);
+      setTrackIndex(trackIndex + 1)
     } else {
-      setTrackIndex(0);
+      setTrackIndex(0)
     }
-  };
+  }, [tracks, trackIndex])
 
-  const startTimer = () => {
-    // Clear any timers already running
-    clearInterval(intervalRef.current);
-
+  const startTimer = useCallback(() => {
     intervalRef.current = window.setInterval(() => {
       if (audioRef.current.ended) {
-        toNextTrack();
+        toNextTrack()
       } else {
-        setTrackProgress(audioRef.current.currentTime);
+        setTrackProgress(audioRef.current.currentTime)
       }
-    }, 1000);
-  };
+    }, 1000)
+
+    return () => {clearInterval(intervalRef.current)}
+  }, [toNextTrack])
 
   useEffect(() => {
     if (isPlaying) {
-      audioRef.current.play();
+      audioRef.current.play()
       startTimer()
     } else {
       clearInterval(intervalRef.current)
-      audioRef.current.pause();
+      audioRef.current.pause()
     }
-  }, [isPlaying]);
+  }, [isPlaying])
 
   useEffect(() => {
     // Pause and clean up on unmount
     const audioRefCur = audioRef.current
     return () => {
-      audioRefCur.pause();
-      // clearInterval(intervalRef.current);
+      audioRefCur.pause()
+      // clearInterval(intervalRef.current)
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
     setTrackIndex(0)
   }, [tracks])
 
-  // Handle setup when changing tracks
   useEffect(() => {
-    audioRef.current.pause();
+    audioRef.current.pause()
 
-    // audioRef.current = new Audio(audioSrc);
-    audioRef.current.src = audioSrc;
-    setTrackProgress(audioRef.current.currentTime);
+    // audioRef.current = new Audio(audioSrc)
+    audioRef.current.src = audioSrc
+    setTrackProgress(audioRef.current.currentTime)
 
     if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      // startTimer();
+      audioRef.current.play()
+      setIsPlaying(true)
+      // startTimer()
     } else {
-      // Set the isReady ref as true for the next pass
-      isReady.current = true;
+      isReady.current = true
     }
-  }, [trackIndex, tracks]);
+  }, [trackIndex, tracks])
 
-  const onScrub = (value: string) => {
+  const onScrub = useCallback((value: string) => {
     // Clear any timers already running
-    clearInterval(intervalRef.current);
-    audioRef.current.currentTime = Number(value);
-    setTrackProgress(audioRef.current.currentTime);
-  }
+    clearInterval(intervalRef.current)
+    audioRef.current.currentTime = Number(value)
+    setTrackProgress(audioRef.current.currentTime)
+  }, [])
 
-  const onScrubEnd = () => {
+  const onScrubEnd = useCallback(() => {
     // If not already playing, start
     if (!isPlaying) {
-      setIsPlaying(true);
+      setIsPlaying(true)
     }
-    startTimer();
-  }
+    startTimer()
+  }, [startTimer])
 
   return (
     <div className="audio_player">
